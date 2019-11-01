efe02c1b5ef0ae47b5187e56d9ce84b276c35b66
AyushKharade <ayush.kharade999@gmail.com>
2019-11-01 15:04:25 -0400

    Added Project files & basic movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //parameters
+    public float swimSpeed;
+    float fastSwimFactor = 1;
+    public float carryWeightFactor=1f;
+    public float jumpForce = 20f;                           // only applicable when on boat or on surface
+
+    int throwableItems;
+
+    //references to objects
+    public Transform facingDirection;
+    Rigidbody rigidbodyRef;
+
+
+    //repspawn point
+    Vector3 respawnPoint;
+    
+
+    //Animator animator;
+
+    // states
+    [HideInInspector]public bool isDead;
+    public bool onSurface;
+    bool onBoat;
+    bool swimming;         // swim animation
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawnPoint = transform.position;
+        rigidbodyRef = GetComponent<Rigidbody>();
+        onSurface = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDead)
+        {
+            PlayerMovement();
+            Abilities();
+        }
+
+        if (isDead)
+        {
+            //coutdown and respawn
+        }
+    }
+
+
+    // movement
+    private void PlayerMovement()
+    {
+
+        //WASD swim:
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += facingDirection.forward * swimSpeed * fastSwimFactor* Time.deltaTime;
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += facingDirection.forward * -swimSpeed * fastSwimFactor*Time.deltaTime;
+        }
+
+        // A & D, strafe, dot product
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += facingDirection.right * -swimSpeed * fastSwimFactor/2*Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += facingDirection.right * swimSpeed *fastSwimFactor/2* Time.deltaTime;
+        }
+
+        // jump
+        if (Input.GetKeyDown(KeyCode.Space) && onSurface)
+        {
+            rigidbodyRef.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        else if (Input.GetKey(KeyCode.Space) && !onSurface)
+        {
+            transform.Translate(Vector3.up * swimSpeed * Time.deltaTime);
+        }
+
+        // go down fast
+        if (Input.GetKey(KeyCode.LeftControl) && !onSurface)
+        {
+            transform.Translate(Vector3.down * swimSpeed * Time.deltaTime);
+        }
+
+
+
+
+        // fast swimming
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            fastSwimFactor = 1.5f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            fastSwimFactor = 1f;
+        }
+    }
+
+    //abilities
+    private void Abilities()
+    {
+        // figurines
+    }
+
+}
---
+++ Assets/Scripts/Underwater.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Underwater : MonoBehaviour
+{
+    public GameObject playerRef;
+    public GameObject camRef;
+
+    // change light for underwater effect (place holder)
+    public Light directionalLightRef;
+    Vector4 originalColor;
+
+    void Start()
+    {
+        originalColor = directionalLightRef.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "MainCamera")
+        {
+            playerRef.GetComponent<Rigidbody>().useGravity=false;
+            playerRef.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            //other.GetComponent<Rigidbody>().AddForce(Vector2.down*0.5f,ForceMode.Impulse);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "MainCamera")
+        {
+            directionalLightRef.color = new Vector4(40 / 255f, 50 / 255f, 142 / 255f, 1);
+            playerRef.GetComponent<Player>().onSurface = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "MainCamera")
+        {
+            directionalLightRef.color = originalColor;
+            //other.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            playerRef.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+}
